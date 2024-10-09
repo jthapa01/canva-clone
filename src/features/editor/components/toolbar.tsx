671086047605e460a7c7a0e1b230d1291470c3b1
jeevan thapa <jeevanthapa@microsoft.com>
2024-10-09 14:42:28 -0400

    Editor Delete tool implementation

--- src/features/editor/components/toolbar.tsx
+++ src/features/editor/components/toolbar.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { FaBold, FaItalic, FaStrikethrough, FaUnderline } from "react-icons/fa";
 import { ActiveTool, Editor, FONT_WEIGHT, FONT_SIZE } from "@/features/editor/types";
-import { ArrowUp, ArrowDown, ChevronDown, AlignLeft, AlignCenter, AlignRight } from "lucide-react";
+import { ArrowUp, ArrowDown, ChevronDown, AlignLeft, AlignCenter, AlignRight, Trash } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Hint } from "@/components/hint";
 import { Button } from "@/components/ui/button";
@@ -316,6 +316,17 @@ export const Toolbar = ({
                         className={cn(activeTool === "opacity" && "bg-gray-100")}
                     >
                         <RxTransparencyGrid className="size-4" />
+                    </Button>
+                </Hint>
+            </div>
+            <div className="flex items-center h-full justify-center">
+                <Hint label="Delete" side="bottom" sideOffset={5}>
+                    <Button
+                        onClick={() => editor?.delete()}
+                        size="icon"
+                        variant="ghost"
+                    >
+                        <Trash className="size-4" />
                     </Button>
                 </Hint>
             </div>
